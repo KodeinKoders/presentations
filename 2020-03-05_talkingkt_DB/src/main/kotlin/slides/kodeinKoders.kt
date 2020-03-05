b8073ddef7c2f82a66a8bd3cdd3b351c7643141b
Salomon BRYS <salomon@kodein.net>
2020-03-05 15:15:27 +0100

    Kotlin Certified

--- 2020-03-05_talkingkt_DB/src/main/kotlin/slides/kodeinKoders.kt
+++ 2020-03-05_talkingkt_DB/src/main/kotlin/slides/kodeinKoders.kt
@@ -1,7 +1,14 @@
 package ws.slides
 
-import kotlinx.css.Color
-import kotlinx.css.backgroundColor
+import kotlinx.css.*
+import kotlinx.css.properties.s
+import kotlinx.css.properties.scale
+import kotlinx.css.properties.transform
+import kotlinx.css.properties.transition
+import react.dom.img
+import styled.css
+import styled.styledDiv
+import styled.styledImg
 import ws.comp.logo
 import ws.kpres.Flip
 import ws.kpres.PresentationBuilder
@@ -10,6 +17,7 @@ import ws.kpres.notes
 
 
 private val infos = SlideInfos(
+        stateCount = 2,
         containerStyle = {
             ".inner-container" {
                 backgroundColor = Color("rgba(0, 0, 0, 0)")
@@ -23,9 +31,28 @@ private val infos = SlideInfos(
         """)
 )
 
-fun PresentationBuilder.kodeinKoders() = slide(infos) {
+fun PresentationBuilder.kodeinKoders() = slide(infos) { props ->
     logo(division = "Koders", href = "https://kodein.net", zoom = 1.0 ) {
         +"painless technology"
+    }
+    styledDiv {
+        css {
+            paddingBottom = 4.em
+        }
+    }
+    styledImg(src = "images/certified-training.svg") {
+        css {
+            height = 4.5.em
+            position = Position.absolute
+            right = 3.em
+            bottom = 2.em
+
+            transition(duration = 0.5.s)
+            if (props.state < 1) {
+                transform { scale(2) }
+                opacity = 0.0
+            }
+        }
     }
 }
 
