214f7c03b3e58409a5c13ab9ea55f1eea43b1e60
Salomon BRYS <salomon.brys@gmail.com>
2022-01-17 13:35:44 +0100

    Skeleton of first pres of 2022

---
+++ 2022-02-06-FossDem-KMP-Mocking/src/jsMain/kotlin/slides/title.kt
@@ -0,0 +1,57 @@
+package slides
+
+import net.kodein.pres.Slide
+import net.kodein.theme.KodeinColor
+import net.kodein.theme.compose.web.Logo
+import net.kodein.theme.compose.web.css
+import org.jetbrains.compose.web.attributes.ATarget
+import org.jetbrains.compose.web.attributes.target
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.*
+
+
+val title = Slide(
+    name = "title"
+) {
+    Img("img/gde.png") {
+        css {
+            height(1.4.em)
+            margin(1.em)
+        }
+    }
+
+    H1({
+        style {
+            fontSize(2.4.em)
+        }
+    }) {
+        Text("Mocking without reflection for Kotlin/Multiplatform")
+    }
+
+    P({
+        css {
+            margin(0.1.em, 0.em, 1.em, 0.em)
+            fontWeight(200)
+        }
+    }) {
+        Text("FossDem Kotlin - 06/02/2022")
+    }
+
+    H3({
+        css {
+            color(KodeinColor.kaumon.css)
+        }
+    }) {
+        Text("Salomon BRYS")
+    }
+
+    A(href = "https://kodein.net", {
+        target(ATarget.Blank)
+        css {
+            margin(0.25.em)
+            fontSize(2.em)
+        }
+    }) {
+        Logo { Text("Koders") }
+    }
+}
